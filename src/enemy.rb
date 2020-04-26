50873a7e5a1f923364f488f9314541237f96c9ad
Brodie Hunt <brodiehunt7@gmail.com>
2020-04-27 08:17:15 +1000

    Styling adjustments to enemy class

--- src/enemy.rb
+++ src/enemy.rb
@@ -1,5 +1,8 @@
+# This class is used to create the enemy object.
+# Contains methods to increase enemies, draw them to the window
+# and check collisions.
 class Enemy
-  attr_reader :enemy_collide 
+  attr_reader :enemy_collide
   def initialize
     @locations = []
     @enemy_collide = false
@@ -9,20 +12,19 @@ class Enemy
     @locations.push([rand(WIDTH), rand(HEIGHT)])
   end
 
-  def draw 
+  def draw
     @locations.each do |index|
-    Square.new(x: index[0] * GRID_SIZE, y: index[1] * GRID_SIZE, size: GRID_SIZE, color: 'red')
+      Square.new(x: index[0] * GRID_SIZE, y: index[1] * GRID_SIZE,
+                 size: GRID_SIZE, color: 'red')
     end
   end
 
   def trump_collide?(head_snake)
     @locations.each do |index|
       if index == head_snake
-        @enemy_collide = true 
+        @enemy_collide = true
         break
       end
     end
   end
-
-  
 end
