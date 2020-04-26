881c7cfba60a6a1b6d032dd58aedd252364c6b08
Brodie Hunt <brodiehunt7@gmail.com>
2020-04-27 08:11:10 +1000

    styling adjustments to class coin

--- src/coin.rb
+++ src/coin.rb
@@ -1,22 +1,21 @@
-require 'ruby2d'
+# This class is responsible for the coin object.
 class Coin
   def initialize
     @x = rand(WIDTH)
     @y = rand(HEIGHT)
   end
-  
 
   def draw
-    coin = Sprite.new('./../media/coin.png', clip_width: 40, time: 300, loop: true, x: @x * GRID_SIZE, y: @y * GRID_SIZE) 
+    Sprite.new('./../media/coin.png', clip_width: 40, time: 300, loop: true,
+                                      x: @x * GRID_SIZE, y: @y * GRID_SIZE)
   end
 
-  def trump_collect?(x, y)
-    @x == x && @y == y 
+  def trump_collect?(collision_x, collision_y)
+    @x == collision_x && @y == collision_y
   end
 
   def change_location
     @x = rand(WIDTH)
     @y = rand(HEIGHT)
   end
-
 end
